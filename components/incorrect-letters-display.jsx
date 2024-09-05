9c52783050bc2cb25b7bb6ceec5fbd6d9703a6b1
Ben McCarthy <ben.mccarthy92@gmail.com>
2024-09-05 19:28:36 +0100

    Update the imports for the images

--- components/incorrect-letters-display.jsx
+++ components/incorrect-letters-display.jsx
@@ -1,19 +1,46 @@
+import React from "react";
+import hangman0 from "../src/images/Hangman-0.jpg";
+import hangman1 from "../src/images/Hangman-1.jpg";
+import hangman2 from "../src/images/Hangman-2.jpg";
+import hangman3 from "../src/images/Hangman-3.jpg";
+import hangman4 from "../src/images/Hangman-4.jpg";
+import hangman5 from "../src/images/Hangman-5.jpg";
+import hangman6 from "../src/images/Hangman-6.jpg";
+import hangman7 from "../src/images/Hangman-7.jpg";
+import hangman8 from "../src/images/Hangman-8.jpg";
+import hangman9 from "../src/images/Hangman-9.jpg";
+import hangman10 from "../src/images/Hangman-10.jpg";
+
+const images = {
+  0: hangman0,
+  1: hangman1,
+  2: hangman2,
+  3: hangman3,
+  4: hangman4,
+  5: hangman5,
+  6: hangman6,
+  7: hangman7,
+  8: hangman8,
+  9: hangman9,
+  10: hangman10,
+};
+
 const IncorrectLettersDisplay = ({ incorrectGuesses, word }) => {
-  const winner = incorrectGuesses.filter((guess) => guess[0] !== word[0]);
+  const incorrectCount = incorrectGuesses.filter(
+    (guess) => guess[0] !== word[0]
+  ).length;
 
   return (
     <>
       <p>
-        {winner.length > 9
+        {incorrectCount > 9
           ? `You Lose! The Word was ${word}`
-          : "Incorrect guesses: " + winner.map((guess) => guess[0]).join(", ")}
+          : "Incorrect guesses: " +
+            incorrectGuesses.map((guess) => guess[0]).join(", ")}
       </p>
       <img
-        src={
-          winner.length > 10
-            ? `../src/Hangman 10.jpg`
-            : `../src/Hangman ${winner.length}.jpg`
-        }
+        src={images[incorrectCount > 10 ? 0 : incorrectCount]}
+        alt={`Hangman state ${incorrectCount}`}
       />
     </>
   );
