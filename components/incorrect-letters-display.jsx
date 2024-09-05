5aa1cb6104ee52a045a0dd14faba96a4bef9ce86
Joshua Martin <martin.joshua2410@gmail.com>
2024-09-05 16:58:58 +0100

    finished error message

--- components/incorrect-letters-display.jsx
+++ components/incorrect-letters-display.jsx
@@ -7,13 +7,13 @@ const IncorrectLettersDisplay = ({ incorrectGuesses, word }) => {
   return (
     <>
       <p>
-        {winner.length > 10
+        {winner.length > 9
           ? `You Lose! The Word was ${word}`
           : "Incorrect guesses: " + winner.join(", ")}
       </p>
       <img
         src={
-          winner.length > 10
+          winner.length > 9
             ? `../hangman-image/Hangman 10.jpg`
             : `../hangman-image/Hangman ${winner.length}.jpg`
         }
