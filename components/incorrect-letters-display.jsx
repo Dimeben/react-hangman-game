a2fe75843ac9140ffe3d71cb4731b80a340fdeba
Ben McCarthy <ben.mccarthy92@gmail.com>
2024-09-05 16:22:51 +0100

    Fixes bugs

--- components/incorrect-letters-display.jsx
+++ components/incorrect-letters-display.jsx
@@ -1,18 +1,19 @@
 const IncorrectLettersDisplay = ({ incorrectGuesses, word }) => {
-  const winner = incorrectGuesses.filter((guess) => {
-    guess === word;
-  });
+  const winner = incorrectGuesses.filter((guess) => guess[0] !== word[0]);
+  console.log(incorrectGuesses);
+  console.log(winner);
+  console.log(word[0]);
 
   return (
     <>
       <p>
-        {winner.length > 9
+        {winner.length > 10
           ? `You Lose! The Word was ${word}`
           : "Incorrect guesses: " + winner.join(", ")}
       </p>
       <img
         src={
-          winner.length > 9
+          winner.length > 10
             ? `../hangman-image/Hangman 10.jpg`
             : `../hangman-image/Hangman ${winner.length}.jpg`
         }
